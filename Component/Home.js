8edcc2842dd76771f5ab80ea49342e5af5dc2583
JyotiKM29 <jyotikumari4442@gmail.com>
2023-07-02 00:19:57 +0530

    Fix svg Town of book

--- Component/Home.js
+++ Component/Home.js
@@ -1,62 +1,40 @@
 import Sidebar from "@/Component/Sidebar";
-import styles from '@/styles/Pages/home.module.scss';
+import styles from "@/styles/Pages/home.module.scss";
 import Image from "next/image";
 
-
-import  AutoSlider from './autoSlider';
+import AutoSlider from "./autoSlider";
 import Link from "next/link";
 
+function Home() {
+  return (
+    <>
+      <div className={styles.book__main}>
+        <Sidebar className={styles.sidebar} />
 
-function Home(){
-    return (
-        <>
-        <div className={styles.book__main}>
-          <Sidebar className={styles.sidebar}/>
-           
-          {/* main page Contain */}
+        {/* main page Contain */}
 
-         
-         <div className={styles.content}>
+        <div className={styles.content}>
+          <div className={styles.heading}>
+            <Image
+              className={styles.image}
+              src="/twn.svg"
+              alt="logo"
+              width={700}
+              height={700}
+            />
+            <div className={styles.row4}>
+              <Link style={{ textDecoration: "none" }} href="/signin">
+                <button className={styles.btn}>Start Reading</button>
+              </Link>
+            </div>
+          </div>
 
-                      <div className={styles.heading}>
-                       {/* <div className={styles.row1}>
-                        Town of 
-                       </div>
-                       <div className={styles.row2}>
-                        Books
-                       </div>
-                       <div className={styles.row3}>
-                        BOOKS WITHOUT HUSTLE IN YOUR TOWN
-                       </div> */}
-                       <Image
-            
-            className={styles.heading__img}
-            src="/twn.svg"
-            alt="logo"
-            width={700}
-            height={700}
-          />
-                       <div className={styles.row4}>
-                       
-                       <Link  style={{"textDecoration":"none"}} href='/signin'>
-                         <button className={styles.btn}> 
-                         Start Reading
-                         </button>
-                       </Link>
-
-                       </div>
-                       </div> 
-
-
-                       {/* <div className={styles.slider}> */}
-                          <AutoSlider  className={styles.slider}/>
-                       {/* </div> */}
-         </div>      
-                
-               
-          
+          {/* <div className={styles.slider}> */}
+          <AutoSlider className={styles.slider} />
+          {/* </div> */}
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
-export default Home
+export default Home;
